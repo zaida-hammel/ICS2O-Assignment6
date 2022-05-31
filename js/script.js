3840fb71a3041f00b6495cd7811b10182242a88f
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-31 17:28:59 +0000

    edited code!

--- js/script.js
+++ js/script.js
@@ -8,10 +8,23 @@
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Assignment6-JS/sw.js", {
     scope: "/ICS2O-Assignment6-JS/",
-  });
+  })
 }
 
-function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+
+/**
+ * Get API info.
+*/
+
+const getPicture = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    console.log(jsonData[0])
+    document.getElementById("api-image").innerHTML = '<img src="' + jsonData[0] + '" alt="API image" class="center" ' + '>'
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getPicture("https://shibe.online/api/shibes?count=[1-100]")
